a18d5a22136800a2b1b2096d78510537be94db74
NambatyaM <keevanstore@gmail.com>
2026-06-30 17:28:03 +0300

    fix: restore logo image from i.ibb.co, re-add to CSP/remotePatterns

--- components/logo.tsx
+++ components/logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export function LogoIcon({ size = 20 }: { size?: number }) {
   return (
     <Image
-      src="/logo.svg"
+      src="https://i.ibb.co/v6h94WVG/keevan-favicon.jpg"
       alt="Keevan Store"
       width={size}
       height={size}
