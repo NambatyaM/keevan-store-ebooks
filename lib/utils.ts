92729cf9d6236bd54ca5f1ce6580a942fe9ff393
NambatyaM <keevanstore@gmail.com>
2026-07-02 02:00:54 +0300

    Fix NoopCache type to preserve return type inference

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,6 @@ export function cn(...classes: Array<string | false | null | undefined>) {
   return classes.filter(Boolean).join(" ");
 }
 
-export function NoopCache<T extends (...args: unknown[]) => unknown>(fn: T): T {
+export function NoopCache<T extends (...args: any[]) => any>(fn: T): T {
   return fn;
 }
