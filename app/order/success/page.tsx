8208b854c8feeeb4bfdc9810e31e17a18ae9c267
NambatyaM <keevanstore@gmail.com>
2026-07-03 04:52:07 +0300

    step 4: test useSearchParams + SimplePage + ErrorBoundary (no state/effects)

--- app/order/success/page.tsx
+++ app/order/success/page.tsx
@@ -4,8 +4,7 @@ import OrderSuccessClient from "./client";
 function LoadingFallback() {
   return (
     <div style={{ textAlign: "center", padding: "40px" }}>
-      <h1>Confirming your payment...</h1>
-      <p>We are verifying your payment with Pesapal.</p>
+      <h1>Loading...</h1>
     </div>
   );
 }
